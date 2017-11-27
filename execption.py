42ccd2f6fdaeb25a4fd24220e4b72fbf889bbc78
saravanakumarjsk <beyousaravana@gmail.com>
2017-11-27 18:40:27 +0530

    execption
    this file explains about execptional handling

---
+++ execption.py
@@ -0,0 +1,15 @@
+try:
+    h = int(input("enter the number: "))
+except:
+    print("this is not a number")
+#hope this will also help, just run this program
+while True:
+    try:
+        num  = eval(input("enter the number:"))
+        print(num/2)
+        break
+    except:
+        print("make sure you enter a number")
+        
+
+######    this is the the new python file
